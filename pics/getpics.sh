a430f83202af421751f7c0484a539b83dd3ef232
Simon Stroh <ps@k0de.de>
2010-10-26 10:03:49 +0200

    removed outdated pic from script

--- pics/getpics.sh
+++ pics/getpics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remove existing pics
-rm -v *png *jpg *gif
+rm -fv *png *jpg *gif
 
 ### Turingmachine
 # Script to get the picture used, as we don't want to include it in the repository until we recieve
@@ -11,13 +11,3 @@ wget -c http://www.ecs.syr.edu/faculty/fawcett/handouts/webpages/pictures/turing
 convert turingMachine.gif -gravity southeast -extent 475x163 tmaschine.png 
 # Remove the .gif we got
 rm turingMachine.gif
-
-
-### DEA
-wget http://upload.wikimedia.org/wikipedia/commons/thumb/9/94/DFA_example_multiplies_of_3.svg/500px-DFA_example_multiplies_of_3.svg.png
-
-
-
-## cleanup
-#latex doesn't like .svg.png
-rename 's/\.svg\.png$/\.png/' *
