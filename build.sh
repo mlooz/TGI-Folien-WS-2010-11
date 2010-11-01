30d7c46a007ad802292e0cb91929fe3020bf2e20
pssimon <ps@bytesized-hosting.com>
2010-11-01 17:49:03 +0100

    more reorganizing ^^

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Fairly stupid script to build .pdf slides
+# TODO: write Makefile
+# 
+# maybe http://www.acoustics.hut.fi/u/mairas/UltimateLatexMakefile/Makefile
+# or better: http://code.google.com/p/latex-makefile/
+#
+# (or our own one) 
+
+
+export TEXINPUTS="$TEXINPUTS:KIT/" 
+for a in `seq 3`; do 
+	pdflatex slides
+done
